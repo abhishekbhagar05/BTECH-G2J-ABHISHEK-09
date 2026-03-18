04a49b847a8019f52449b1fcfff6fc8baf5b9222
abhishekbhagar05 <abhishekbhagar05@gmail.com>
2026-03-17 21:57:36 -0700

    Create wap to count no of digits in a no .c

---
+++ wap to count no of digits in a no .c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+
+int main() {
+    long long num;
+    int count = 0;
+
+  
+    printf("Enter an integer: ");
+   
+    if (scanf("%lld", &num) != 1) {
+        printf("Invalid input. Please enter a number.\\n");
+        return 1;
+    }
+
+    
+    if (num == 0) {
+        count = 1;
+    } else {
+        
+        if (num < 0) {
+            num = -num;
+        }
+
+        
+        while (num != 0) {
+            num = num / 10;  
+            count++;      
+        }
+    }
+
+    printf("Number of digits: %d\\n", count);
+
+    return 0;
+}
