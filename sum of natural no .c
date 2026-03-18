c0754447cde7593cf17920d5375b83a2d1c2df12
abhishekbhagar05 <abhishekbhagar05@gmail.com>
2026-03-17 21:35:06 -0700

    Create sum of natural no .c

---
+++ sum of natural no .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    
+    for (i = 1; i <= n; ++i) {
+        sum += i; // equivalent to sum = sum + i;
+    }
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
